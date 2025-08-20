f21d92380d99f0ba77c60f60222885bea946d608
Ritme54 <tmdwns5456@naver.com>
2025-08-20 15:44:12 +0900

    (2025.08.20)퇴각 검색

--- ConsoleApplication1/program.cpp
+++ ConsoleApplication1/program.cpp
@@ -1,86 +1,156 @@
 ﻿#include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
-#define SIZE 7
+#define SIZE 8
+
 
 
 using namespace std;
 
-
-int parent[SIZE];
-
-
-int find(int x)
+class Kruskal
 {
-	//배열의 인덱스와 값이 같다면 root node를 발견
-	if (parent[x]==x)
+private:
+	class Edge
 	{
-		return x;
-	}	//자기 자신을 부모로 가지면 루트(대표)
-	else
+	private:
+		int vertexX;
+		int vertexY;
+		int weight;
+	public:
+		Edge(int vertexX, int vertexY, int weight)
+		{
+			this->vertexX = vertexX;
+			this->vertexY = vertexY;
+			this->weight = weight;
+
+		}
+
+		const bool& operator<(const Edge& edge) const
+		{
+
+
+			return weight < edge.weight;
+		}
+
+		const int& X() { return vertexX; }
+		const int& Y() { return vertexY; }
+		const int& W() { return weight; }
+
+
+	};
+
+	vector<Edge>nodeList;
+
+	int parent[SIZE];
+	void ufInit()
 	{
-	//부모 노드의 번호를 전달하면서, root Node를 찾을 때까지 재귀 함수를 호출하여 반복합니다.
-		return parent[x] = find(parent[x]);//경로 압축:한번에 루트로 부모 갱신.
+		for (int i = 0; i < SIZE; ++i)
+		{
+			parent[i] = i;
+		}
+	}
+	int ufFind(int x)
+	{
+		if (parent[x] == x) return x;
+		return parent[x] = ufFind(parent[x]); // 경로 압축
+	}
+
+	bool ufUnion(int a, int b)
+	{
+		a = ufFind(a);
+		b = ufFind(b);
+		if (a == b) return false; // 같은 집합이면 사이클 발생 → 채택 금지
+
+		return true;
+	}
+
+public:
+	Kruskal()
+	{
 
 	}
 
-}
-void Union(int x, int y)
-{
-	
-	x = find(x);
-	y = find(y);
-	if (x == y)return;
-	if (x<y)
+	void insert(int vertexX, int vertexY, int weight)
 	{
-		parent[y] = x;
+		Edge edge(vertexX, vertexY, weight);
+		nodeList.push_back(edge);
+
+
 	}
-	else
+
+	void calculate()
 	{
-		parent[x] = y;
-	}
-	
-}
-bool same(int x, int y)
-{
-	return find(x) == find(y);
-
-}
+		sort(nodeList.begin(), nodeList.end());
 
 
-int main()
-{
+		// 2) 분리 집합 초기화
+		ufInit();
 
-#pragma region 서로소 집합
-	//서로 공통된 원소를 가지고 있지 않은 두개 이상의 집합을 의미합니다.
+		// 3) 간선을 순회하며 MST 구성
+		vector<Edge> mst;
+		int cost = 0;
 
-	//union(합집합) : 두 집합을 하나로 합치는 연산
-	//find(찾기) : 특정한 원소가 속한 집합이 어떤 집합인지 알려주는 연산이다.
-	// 
-	// 1. 합집합 연산을 확인하여, 서로 연결된 두 노드를 확인합니다.
-		//1) A 와 B의 루트 노드를 A' 와 B'를 각각 찾습니다.
-	//2) A' 와 B'의 부모 노드를 설정합니다.
+		for (int i = 0; i < nodeList.size(); i++)
+		{
+			//cout << nodeList[i].X() << endl;
+			//cout << nodeList[i].Y() << endl;
+			//cout << nodeList[i].W() << endl;			
 
-	//2. 모든 합집합 연사를 처리할 때까지 1번 과정을 반복합니다.
-	
-	for (int i = 0; i < SIZE; i++)
+			int u = nodeList[i].X();
+			int v = nodeList[i].Y();
+			int w = nodeList[i].W();
+
+			// 두 정점이 다른 집합일 때만 간선 채택(사이클 방지)
+			if (ufUnion(u, v))
+			{
+				mst.push_back(nodeList[i]);
+				cost += w;
+			}
+		}
+		cout << "MST 간선 목록 (X - Y : W)\n";
+		for (size_t i = 0; i < mst.size(); ++i)
+		{
+			cout << mst[i].X() << " - " << mst[i].Y() << " : " << mst[i].W() << "\n";
+		}
+		cout << "MST 총 가중치: " << cost << "\n";
+
+	};
+
+
+	int main()
 	{
-		parent[i] = i;
-	}
-	
 
-	Union(0, 1);
-	Union(0, 2);
-	Union(0, 3);
+#pragma region 최소 신장 트리
+		//그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는 부분 그래프로,
+		// 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
 
-	Union(4, 5);
-	Union(5, 6);
+		// 그래프의 정점의 수가n개 일때, 간선의 수는 n-1개 입니다.
 
-	cout << "same : " << same(1, 3) << endl;
-	cout << "same : " << same(1, 6) << endl;
+		//1. 정렬된 순서에 맞게 그래프에 포함시킵니다.
+		//2. 포함시키기 전에 사이클이 형성되는지 확인합니다.
+		//3.사이클을 형성하는 경우 간선을 포함하지 않습니다.
 
+		Kruskal kruskal;
 
+		kruskal.insert(1, 7, 12);
+		kruskal.insert(4, 7, 13);
+
+		kruskal.insert(2, 4, 23);
+		kruskal.insert(2, 5, 65);
+
+		kruskal.insert(1, 2, 71);
+		kruskal.insert(5, 7, 79);
+
+		kruskal.insert(1, 4, 30);
+		kruskal.insert(1, 5, 15);
+		kruskal.insert(3, 5, 18);
+
+		kruskal.insert(3, 6, 36);
+		kruskal.insert(5, 6, 44);
+
+		kruskal.calculate();
 
 #pragma endregion
 
@@ -89,5 +159,5 @@ int main()
 
 
 
-	return 0;
-}
+		return 0;
+	}
